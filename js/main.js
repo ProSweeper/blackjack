fbf3d6939939f6c6824458b9173c3be87bb5cbfc
ProSweeper <dylan.doucet28@gmail.com>
2022-10-08 15:34:44 +0000

    catch origin up to remote for laptop work

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 /*-------constants-------*/
 // The deck should be an array of objects, this way it has access to array methods
-    
+const SUITS = ['s', 'c', 'd', 'h'];
+const RANKS = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const PHASES = ['betting', 'player action', 'dealer action']
 // card objects should hold value(int), suit(string), showing(boolean) values 
+
 
 /*----state variables----*/
 // the player will be an object housing an array of card objects, hand value, bank value,      
 // the dealer will be an object housing an array of card objects
 // the pot holding the bet money
-
+let player;
+let dealer;
+let shuffledDeck;
+let pot;
 /*----Cached Elements----*/
 // chip buttons
 // hit and stand buttons
@@ -21,6 +27,7 @@
 // deal cards button
 
 /*-------Functions-------*/
+in
 // check the players bank balance and make sure its not 0
 // when the player goes to submit a bet make sure it is not larger than the bank value
 // subtract the bet amount from the players bank and add it to the pot
