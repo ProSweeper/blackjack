c8b81a2aa62b7233ed001f62cc32614b1bb116d0
ProSweeper <dylan.doucet28@gmail.com>
2022-10-09 00:40:27 +0000

    flesh out Player class with methods and properties

--- js/main.js
+++ js/main.js
@@ -46,19 +46,47 @@ class Player {
         this.handValue = 0;
         this.blackJack = false;
         this.bust = false;
+        this.aceCount = 0;
     }
 
-    dealCard () {
+    dealCards () {
+        // reset the bust and blackjack since this is the 2 cards dealt
+        this.blackJack = false;
+        this.bust = false;
         this.hand.push(shuffledDeck.pop());
         this.hand.push(shuffledDeck.pop());
-        if (this.handValue === 21) {
-            this.blackJack = true;
-        }
+        this.evalHand();
     }
     
     hit () {
         this.hand.push(shuffledDeck.pop());
+        this.evalHand();
     }
+
+    evalHand () {
+        // cards look like {face: 'c04', value: 4}
+        this.hand.forEach((card) => {
+            this.handValue += card.value;
+            // add to the ace count if we need to 
+            if (card.value === 11) this.aceCount++;
+        });
+        // blackjacks can only occur in the first two cards dealt
+        if (this.handValue === 21 && this.hand.length === 2) {
+            this.blackJack = true;
+        }
+        // since ace can be worth 11 or 1 we must reduce handvalue by 10 if it goes over 21
+        // and an ace is in the hand
+        while (this.aceCount && this.handValue > 21) {
+            this.handValue -= 10;
+            this.aceCount -= 1;
+        }
+        if (this.handValue > 21 ) this.bust = true;
+        render();
+    }
+}
+
+class Dealer extends Player {
+    
 }
 
 function resetPot() {
@@ -80,12 +108,12 @@ function getMainDeck() {
                 value: Number(rank) || (rank === 'A' ? 11 : 10)
             };
             // push the card to the deck
+            // card looks something like {face: 'c04', value: 4}
             deck.push(card);
         })
     })
     return deck;
 }
-
 
 function getShuffledDeck() {
     // we want to use a temp deck so we don't alter our maindeck ever
